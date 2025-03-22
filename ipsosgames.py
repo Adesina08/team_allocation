7da3b389c4eba8ec3dfcf29d7ac1e94145377173
Adesina08 <153903110+Adesina08@users.noreply.github.com>
2025-03-22 19:04:55 +0100

    Update ipsosgames.py

--- ipsosgames.py
+++ ipsosgames.py
@@ -648,7 +648,7 @@ def standings_page():
 
     except FileNotFoundError:
         st.error("No gameweek data available yet! Check back after the first game (4th April 2025).")
-        st.image("images/coming_soon.jpg")
+        st.image("images/coming_soon.jpg", width=400)
     except Exception as e:
         st.error(f"Error processing data: {str(e)}")
 
